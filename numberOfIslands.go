6a0a375f987bb33d4940cbce3a83aafb1860ce93
Mathias Valdebenito <mathias.valdebenito@beetrack.com>
2022-08-17 11:25:48 -0400

    manage writing of files

--- numberOfIslands.go
+++ numberOfIslands.go
@@ -3,21 +3,55 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 func main() {
 	files, err := ioutil.ReadDir("./matrices/")
-	check(len(files), err)
+	check(err)
+	checkDirIsEmpty(len(files))
 
 	for _, f := range files {
-		fmt.Println(f.Name())
+		dat := openFile(f.Name())
+		parseFile(dat) // retornar estructura
+		// resolver problema
+		f := createFile("numberOfIslands_<id>.txt")
+		writeFileAndClose("2", f)
 	}
 }
 
-func check(lenght int, e error) {
+func check(e error) {
 	if e != nil {
 		panic(e)
-	} else if lenght == 0 {
+	}
+}
+
+func checkDirIsEmpty(lenght int) {
+	if lenght == 0 {
 		panic("oh no! folder is empty")
 	}
+}
+
+func openFile(fileName string) string {
+	dat, err := os.ReadFile("./matrices/" + fileName)
+	check(err)
+	return string(dat)
+}
+
+func createFile(name string) *os.File {
+	f, err := os.Create("./respuestas/" + name)
+	check(err)
+	//defer f.Close()
+	return f
+}
+
+func writeFileAndClose(data string, f *os.File) {
+	d := []byte(data)
+	_, err := f.Write(d)
+	check(err)
+	defer f.Close()
+}
+
+func parseFile(dat string) {
+	fmt.Print(dat + "\n")
 }
