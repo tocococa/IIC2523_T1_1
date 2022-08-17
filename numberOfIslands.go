96515aa9b4a3901734f3acdcc41ad657e856b393
Mathias Valdebenito <mathias.valdebenito@beetrack.com>
2022-08-17 10:45:22 -0400

    check if dir is empty

--- numberOfIslands.go
+++ numberOfIslands.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
+	"fmt"
+	"io/ioutil"
 )
 
 func main() {
-    files, err := ioutil.ReadDir("./matrices/")
-	check(err)
+	files, err := ioutil.ReadDir("./matrices/")
+	check(len(files), err)
 
 	for _, f := range files {
 		fmt.Println(f.Name())
 	}
 }
 
-func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+func check(lenght int, e error) {
+	if e != nil {
+		panic(e)
+	} else if lenght == 0 {
+		panic("oh no! folder is empty")
+	}
 }
